4b1db567dec7416eb1e0dd4b3e02fca9cdb79e54
nforsg <nforsg@kth.se>
2023-06-18 13:50:40 +0200

    simple script for learning pytest

--- Pytests/func_mania.py
+++ Pytests/func_mania.py
@@ -2,22 +2,26 @@ import  numpy as np
 import random as rn
 
 class _Animal():
-    def __init__(self, species, mood, hunger, health, diet):
+    def __init__(self, species, mood, hunger):#, health, diet):
         self.species = species
         self.mood = mood
         self.hunger = hunger
-        self.health = health
-        self.diet = diet
+        #self.health = health
+        #self.diet = diet
 
 class Pasture():
-    def __init__(self, land = np.zeros((100,100)), temperature = 25):
+    '''Class that represents a pasture, filled with animals'''
+    def __init__(self, land = np.zeros((25,25)), temperature = 25):
+
+        '''Initialiizng properties of pasture'''
+
         self.land = land
         self.temp = temperature
 
-    def counter(self, array):
+    def counter(self):
 
         '''Counting the number of animals in the pasture'''
-        
+
         count = 0
         for i in range(np.shape(self.land)[0]):
             for j in range(np.shape(self.land)[1]):
@@ -25,15 +29,57 @@ class Pasture():
                     count += 1
         return count
     
-    #def add_animal(x,y, species, mood, hunger, health, diet):
+    def add_animal(self, x,y, species, mood, hunger):#, health, diet):
+
+        '''Adding amnimal to the pasture'''
+
+        anm = _Animal(species, mood, hunger)#, health, diet)
+        if anm.species == "Sheep":
+            self.land[x,y] = 1
+
+        elif anm.species == "Cow":
+            self.land[x,y] = 2
+
+        elif anm.species == "Lama":
+            self.land[x,y] == 3
         
+        elif anm.species == "Bull":
+            self.land[x,y] = 6
 
+        else:
+            print('Not a valid species of animal')
 
+        if anm.mood in range(10):
+            if anm.hunger in range(10):
+                print('The ', anm.species, ' is in a bad mood due to hunger.')
+            else: 
+                print('The ', anm.species, ' is in a bad mood due to the weather')
+        else: 
+            print('The ', anm.species , ' is doing fine and is in a good mood.')
+        
+        print(self.land)
 
+        return self.land
+    
 
+class Functions(): 
+    def __init__(self) -> None:
+        pass
+    
+    def counter(self, array): 
+        
+        '''Counting number of elements in array'''
+        
+        counter = 0
+
+        for i in range(len(array)):
+            counter += 1 
+        return counter
 
     def mergesort(self, array):
+
         '''Sorts the array with divide & conquer in O(nlogn) time'''
+
         if len(array) > 1:
             splitter_index = len(array)//2
             left_part = array[:splitter_index]
--- Pytests/test_funcs.py
+++ Pytests/test_funcs.py
@@ -1,20 +1,22 @@
 from func_mania import Pasture
+from func_mania import Functions
 
 
 
 arr1 = [1,2,3,4,5,6,7,8]
 arr2 = [10,9,8,7,6,5,4,3,2,1]
-func = Pasture()
+func = Functions()
+pst = Pasture()
 
-def test_size():
-    '''Testig the array-size function called counter'''
+def test_size_functions():
+    '''Testig the array-size function called counter in class Functions'''
     
     assert func.counter(arr1) == len(arr1)
     
     assert func.counter(arr2) == len(arr2) 
 
-def test_sort():
-    '''Testing the mergesort'''
+def test_sort_functions():
+    '''Testing the mergesort in class Functions'''
    
     for i in range(1, len(arr1)):
         assert func.mergesort(arr1)[i-1] <= func.mergesort(arr1)[i]
@@ -22,3 +24,18 @@ def test_sort():
     for j in range(1, len(arr2)):
         assert func.mergesort(arr2)[j-1] <= func.mergesort(arr2)[j]
 
+def test_pasture():
+    '''Testing funcitonality of Pasture class'''
+
+    assert pst.counter() == 0
+
+    pst.add_animal(10,10, 'Cow', 15, 15)
+    assert pst.land[10,10] == 2
+
+    pst.add_animal(20,20, 'Sheep', 8,8)
+    assert pst.land[20,20] == 1
+    
+    pst.add_animal(18,18, 'Bull', 12,12)
+    assert pst.land[18,18] == 6
+
+    pst.counter() == 3
